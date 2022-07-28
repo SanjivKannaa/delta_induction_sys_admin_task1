6c49b9ae6fede076d304ae53e71b8779b00aa750
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-07-28 23:22:45 +0530

    added login logout

--- web dev/task_3_2/main.py
+++ web dev/task_3_2/main.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 @app.route('/')
 def function():
     if  request.cookies.get('loginstatus') == 'True':
-        return render_template('feed.html')
+        return 'feed for user : '+request.cookies.get('login_username')
+        #return render_template('feed.html')
         # feed()
     else:
         return render_template('login.html')
@@ -28,11 +29,8 @@ def developer_information():
 def index():
     return render_template('index.html')
 
-@app.route('/login')
-def login_call():
-    return render_template('login.html')
 
-@app.route('/login/auth/', methods=["POST", "GET"])
+@app.route('/login', methods=["POST", "GET"])
 def login_validation():
     if request.method == 'GET':
         return render_template('login.html')
@@ -43,7 +41,7 @@ def login_validation():
         #print('\n\n\n\n{}\n\n{}\n\n\n\n'.format(len(rollno), len(password)))
         #return "username = {} password = {}".format(rollno, password)
         if content[rollno] == password:
-            res = make_response('login_success.html')
+            res = make_response(render_template('login_success.html'))
             username = database.get_user_info(rollno)['username']
             res.set_cookie('loginstatus', 'True')
             res.set_cookie('login_rollno', rollno)
@@ -51,6 +49,33 @@ def login_validation():
             return res
         else:
             return make_response(render_template("login.html"))
+
+
+@app.route('/logout', methods=["POST", "GET"])
+def logout():
+    if request.method == 'GET':
+        res = make_response(render_template('login.html'))
+        res.set_cookie('loginstatus', "false")
+        res.set_cookie('login_rollno', '')
+        res.set_cookie('login_username', '')
+        return res
+
+@app.route('/forgot_password', methods=["GET", "POST"])
+def forgot_password():
+    if request.method == "GET":
+        pass
+    else:
+        pass
+
+
+@app.route('/feed', methods=["GET"])
+def feed():
+    if  request.cookies.get('loginstatus') == 'True':
+        return render_template('feed.html')
+    else:
+        return render_template('login.html')
+
+
 
 '''
 @app.route('/login_bypass', methods = ['POST', 'GET'])
